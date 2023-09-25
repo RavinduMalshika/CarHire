3cde50a8c24a23ea7b7fa96f18ae420364610520
RavinduMalshika <ravindu.malshika@gmail.com>
2023-09-25 21:08:59 +0530

    Report Directory Creation

--- src/main/java/lk/ijse/carhire/Main.java
+++ src/main/java/lk/ijse/carhire/Main.java
@@ -1,10 +1,19 @@
 package lk.ijse.carhire;
 
-import lk.ijse.carhire.util.SessionFactoryConfiguration;
-import org.hibernate.Session;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
+        File reportsDir = new File("Reports");
+        if (!reportsDir.exists()){
+            reportsDir.mkdirs();
+            new File("Reports/Customer Reports").mkdirs();
+            new File("Reports/Car Reports").mkdirs();
+            new File("Reports/Rent Reports").mkdirs();
+        }
+
         Launcher.main(args);
     }
 }
